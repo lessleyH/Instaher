a51cea6e56627498d7309cce50e5d21115304837
Lessley Hernandez <lessley@fb.com>
2018-07-12 19:18:31 -0700

    camera running

--- app/src/main/java/com/kalicalitally/instaher/HomeActivity.java
+++ app/src/main/java/com/kalicalitally/instaher/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.kalicalitally.instaher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
 import java.util.List;
@@ -33,40 +36,59 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         descriptionInput = findViewById(R.id.etDescription);
-        createBttn = findViewById(R.id.btnCreate);
-        refreshBttn = findViewById(R.id.btnRefresh);
+                createBttn = findViewById(R.id.btnCreate);
+                refreshBttn = findViewById(R.id.btnRefresh);
 
-        createBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String description = descriptionInput.getText().toString();
-                final ParseUser user = ParseUser.getCurrentUser();
-                    //add the image later
+                createBttn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+//                        final String description = descriptionInput.getText().toString();
+//                        final ParseUser user = ParseUser.getCurrentUser();
+//                            //add the image later
+//
+//                        //TODO: this is still hard coded
+//                        final File file = new File("yo ");
+//
+//                        final ParseFile parseFile = new ParseFile(file);
+//                        createPost(description, parseFile, user);
 
-                //TODO: this is still hard coded
-                final File file = new File("yo ");
+                        Intent i  = new Intent(HomeActivity.this, CameraActivity.class);
+                        startActivity(i);
 
-                final ParseFile parseFile = new ParseFile(file);
-
-                createPost(description, parseFile, user);
-            }
-        });
+                        }
+         });
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.frag_home, HomeFeed).commit();
+//
+//        BottomNavigationView navigation= (BottomNavigationView)findViewById(R.id.bottom_navigation);
+//        navigation.setOnNavigationItemSelectedListener(bottomNavigationView);
 
         refreshBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 loadTopPosts();
-            }
+                }
         });
-
-
-
-        loadTopPosts();
     }
 private void createPost(String description, ParseFile imageFile, ParseUser user){
         //TODO: create and save post
-}
 
+    final Post newPost = new Post();
+    newPost.setDescription(description);
+  //  newPost.setImage(imageFile);
+    newPost.setUser(user);
+
+    newPost.saveInBackground(new SaveCallback() {
+        @Override
+        public void done(ParseException e) {
+            if (e == null) {
+                Log.d("HomeActivity", "Create Post called and done :)!!");
+            } else {
+                e.printStackTrace();
+            }
+        }
+    });
+}
 
 
 private void loadTopPosts() {
